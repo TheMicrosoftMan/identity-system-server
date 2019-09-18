b99584d66a97143045a48e8ccf8effa0c9c2e374
TheMicrosoftMan <yurazelenko@windowslive.com>
2019-09-18 11:44:18 +0300

    initial commit

---
+++ utils/analisator.js
@@ -0,0 +1,27 @@
+const Classifier = require("wink-naive-bayes-text-classifier");
+const nbc = Classifier();
+const nlp = require("wink-nlp-utils");
+
+const data = require("../data/data.json");
+
+nbc.definePrepTasks([
+  nlp.string.tokenize0,
+  nlp.tokens.removeWords,
+  nlp.tokens.stem
+]);
+
+nbc.defineConfig({
+  considerOnlyPresence: true,
+  smoothingFactor: 0.5
+});
+
+nbc.defineConfig({ considerOnlyPresence: true, smoothingFactor: 0.5 });
+for (let i = 0; i < data.data.length; i++) {
+  nbc.learn(data.data[i].quote, data.data[i].author);
+}
+
+nbc.consolidate();
+
+console.log("Training completed");
+
+module.exports = nbc.predict;
