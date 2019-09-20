c6c9a8bf22c2cd4eb0f56a0d15791549ac3a7e2a
TheMicrosoftMan <yurazelenko@windowslive.com>
2019-09-20 11:57:37 +0300

    Add new datasets, add console logging

--- analisator/index.js
+++ analisator/index.js
@@ -20,16 +20,21 @@ nbc.defineConfig({
   smoothingFactor: 0.5
 });
 
+console.log("Training start.");
+console.time("Training");
+
 pathsToData.forEach(trainingFile => {
   const data = require(trainingFile);
   for (let i = 0; i < data.data.length; i++) {
-    nbc.learn(data.data[i].quote, data.data[i].author);
+    nbc.learn(data.data[i].text, data.data[i].author);
   }
 });
 
 nbc.consolidate();
 
-console.log("Training completed");
+console.timeEnd("Training");
+console.log("Training complete.");
+console.log("Naive Bayes text classifier is ready.");
 
 const predict = text => {
   const predictAuthor = nbc.predict(text);
