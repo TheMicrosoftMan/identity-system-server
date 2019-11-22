f305bad992aaa04b42f079b34cbbc78b4f1d3004
TheMicrosoftMan <yurazelenko@windowslive.com>
2019-11-22 11:52:01 +0200

    Add PORT from process

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const app = express();
 const cors = require("cors");
 const fileUpload = require("express-fileupload");
 
+require("dotenv").config();
+
 app.use(express.json());
 app.use(fileUpload());
 app.use(cors());
@@ -10,6 +12,6 @@ app.use(cors());
 app.use("/api/analis", require("./api/analis"));
 app.use("/api/datasets", require("./api/datasets"));
 
-app.listen(3055, () => {
+app.listen(process.env.PORT, () => {
   console.log("Server is working");
 });
